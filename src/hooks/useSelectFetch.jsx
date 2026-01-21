d2bdcece224b26d6c42885059ab3f0fc1b75bb7e
S74RK1113R <randyes16@gmail.com>
2026-01-21 00:55:31 -0500

    add commit

--- src/hooks/useSelectFetch.jsx
+++ src/hooks/useSelectFetch.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
+/* Hook for the select tags*/
 
 export const useSelectFetch = (url) => {
   const [data, setData] = useState([]);
